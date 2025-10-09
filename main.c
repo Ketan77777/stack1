190f1e7ab11dac7e35bf759af24aa726b5bc2d6f
Ketan77777 <ketanshinge2006@gmail.com>
2025-10-09 16:41:59 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,32 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<ctype.h>
-#include<math.h>
-#define SIZE 10
+#define SIZE 5
 struct stack
 {
-int top;
-float data[SIZE];
+    int top;
+    int data[SIZE];
 };
-typedef struct stack STACK ;
-void push(STACK *s,char item)
+typedef struct stack STACK;
+void push(STACK*s,int item)
 {
-    s->data[++(s->top)]=item;
+if(s->top==SIZE-1)
+    printf("\n stack overflow");
+    else
+        {
+        s->top=s->top+1;
+        s->data[s->top]=item;
+        }}
+
+void pop(STACK*s)
+{
+if(pop==-1)
+    printf("\n stack underflow");
+    else
+        {
+            printf("\n element poped is %d",s->data[s->top]);
+        s->top=s->top-1;
+        }}
+void display(STACK S)
+{
+    int i;
+if(S.top==-1)
+    printf("\n stack empty");
+    else{
+            printf("\n stack contants are \n");
+            for(i=S.top;i>=0;i--)
+        printf("%d\n",S.data[i]);
+        }}
+        int main()
+        {
+            int ch,item;
+            STACK S;
+            S.top=-1;
+            for(;;)
+            {
+                printf("\n1.push\n2.pop\n3.display\n4.exit");
+                printf("\nread choice:");
+                scanf("%d\n",&ch);
+            }
+            switch(ch){
+            case1:
+                printf("read element to be pushed:");
+                scanf("%d",&item);
+                push(&S,item); break;
+             case2:
+                 pop(&S); break;
+                 case3:
+                 display(S); break;
+default: exit (0);
 }
-float pop(STACK *s)
-{
- return s->data[(s->top)--];
+return 0;
+
 }
-int preced(char symbol)
-{
-    switch (symbol)
-    {
-    case '+':
-    case '-':return 1;
-    case '*':
-    case '/':return 3;
-    case '^':return 4;
-    }
-}
-void infixtopostfix(STACK *s,char infix[20])
-{
